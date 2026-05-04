1d9786b23641c435882baf50c898caf51283fc5b
Alexandr Novosyolov <aain34@mail.ru>
2026-05-04 20:34:24 +0300

    chore: finalize auth module integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import { router } from './app/providers/router.tsx'
-import { RouterProvider } from 'react-router'
+import { App } from '@/app'
+import '@/app/assets/styles/index.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import path from 'path'
+import svgr from 'vite-plugin-svgr'
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@app': path.resolve(__dirname, './src/app'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@widgets': path.resolve(__dirname, './src/widgets'),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @import "@/app/assets/styles/_variables.scss";
+        @import "@/app/assets/styles/_mixins.scss";
+      `
+      }
+    }
+  }
 })
